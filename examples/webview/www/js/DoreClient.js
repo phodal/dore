637bd83589de848bc5c622824677843262fa18a4
travelgeezer <travelgeezer@icloud.com>
2017-12-18 22:46:07 +0800

    Try use console bridge

--- examples/webview/www/js/DoreClient.js
+++ examples/webview/www/js/DoreClient.js
@@ -1,20 +1,20 @@
 /*global define */
-(function (global, factory) {
-  "use strict";
-  if (typeof module === "object" && typeof module.exports === "object") {
-    module.exports = global.document ?
-      factory(global, true) :
-      function (w) {
-        if (!w.document) {
-          throw new Error("jQuery requires a window with a document");
-        }
-        return factory(w);
-      };
+(function(global, factory) {
+  'use strict';
+  if (typeof module === 'object' && typeof module.exports === 'object') {
+    module.exports = global.document
+      ? factory(global, true)
+      : function(w) {
+          if (!w.document) {
+            throw new Error('jQuery requires a window with a document');
+          }
+          return factory(w);
+        };
   } else {
     factory(global);
   }
+})(typeof window !== 'undefined' ? window : this, function(window, noGlobal) {
 
-}(typeof window !== "undefined" ? window : this, function (window, noGlobal) {
   function postMessage(action, payload) {
     var message = JSON.stringify({
       action: action,
@@ -28,19 +28,21 @@
   }
 
   function getAsyncData(action, payload) {
-    return new Promise(function (resolve, reject) {
+    return new Promise(function(resolve, reject) {
       function listener(event) {
         event.target.removeEventListener('message', listener, false);
         resolve(JSON.parse(event.data));
       }
 
       window.document.addEventListener('message', listener, false);
-      window.postMessage(JSON.stringify({
-        action: action,
-        payload: payload
-      }));
+      window.postMessage(
+        JSON.stringify({
+          action: action,
+          payload: payload
+        })
+      );
 
-      setTimeout(function () {
+      setTimeout(function() {
         reject('timeout');
       }, 3000);
     });
@@ -51,117 +53,291 @@
   }
 
   var DoreClient = {
-    getAppVersion: function () {
-      return getAsyncData('DEVICE_INFO', {type: 'APP_VERSION'});
+    getAppVersion: function() {
+      return getAsyncData('DEVICE_INFO', { type: 'APP_VERSION' });
     },
-    getUniqueID: function () {
-      return getAsyncData('DEVICE_INFO', {type: 'DEVICE_ID'});
+    getUniqueID: function() {
+      return getAsyncData('DEVICE_INFO', { type: 'DEVICE_ID' });
     },
-    getBrand: function () {
-      return getAsyncData('DEVICE_INFO', {type: 'BRAND'});
+    getBrand: function() {
+      return getAsyncData('DEVICE_INFO', { type: 'BRAND' });
     },
-    getModel: function () {
-      return getAsyncData('DEVICE_INFO', {type: 'MODEL'});
+    getModel: function() {
+      return getAsyncData('DEVICE_INFO', { type: 'MODEL' });
     },
-    getSystemName: function () {
-      return getAsyncData('DEVICE_INFO', {type: 'SYSTEM_NAME'});
+    getSystemName: function() {
+      return getAsyncData('DEVICE_INFO', { type: 'SYSTEM_NAME' });
     },
-    isEmulator: function () {
-      return getAsyncData('DEVICE_INFO', {type: 'IS_EMULATOR'});
+    isEmulator: function() {
+      return getAsyncData('DEVICE_INFO', { type: 'IS_EMULATOR' });
     },
-    isTablet: function () {
-      return getAsyncData('DEVICE_INFO', {type: 'IS_TABLET'});
+    isTablet: function() {
+      return getAsyncData('DEVICE_INFO', { type: 'IS_TABLET' });
     },
-    showToast: function (message, duration, position) {
-      return invoke('TOAST', {options: {duration: duration, position: position}, message: message});
+    showToast: function(message, duration, position) {
+      return invoke('TOAST', {
+        options: { duration: duration, position: position },
+        message: message
+      });
     },
-    showDatePicker: function (options) {
+    showDatePicker: function(options) {
       options.TYPE = 'DATE_PICKER';
       return getAsyncData('DATE_PICKER', options);
     },
-    setBadge: function (badgeNumber) {
-      return invoke('BADGE', {type: 'SET_BADGE', badgeNumber: badgeNumber});
+    setBadge: function(badgeNumber) {
+      return invoke('BADGE', { type: 'SET_BADGE', badgeNumber: badgeNumber });
     },
-    getBadge: function (badgeNumber) {
-      return getAsyncData('BADGE', {type: 'GET_BADGE', badgeNumber: badgeNumber});
+    getBadge: function(badgeNumber) {
+      return getAsyncData('BADGE', {
+        type: 'GET_BADGE',
+        badgeNumber: badgeNumber
+      });
     },
-    addBadge: function (badgeNumber) {
-      return invoke('BADGE', {type: 'ADD_BADGE', badgeNumber: badgeNumber});
+    addBadge: function(badgeNumber) {
+      return invoke('BADGE', { type: 'ADD_BADGE', badgeNumber: badgeNumber });
     },
-    minusBadge: function (badgeNumber) {
-      return invoke('BADGE', {type: 'MINUS_BADGE', badgeNumber: badgeNumber});
+    minusBadge: function(badgeNumber) {
+      return invoke('BADGE', { type: 'MINUS_BADGE', badgeNumber: badgeNumber });
     },
-    cleanBadge: function (badgeNumber) {
-      return invoke('BADGE', {type: 'CLEAN_BADGE', badgeNumber: badgeNumber});
+    cleanBadge: function(badgeNumber) {
+      return invoke('BADGE', { type: 'CLEAN_BADGE', badgeNumber: badgeNumber });
     },
-    hideKeyboard: function () {
-      return invoke('KEYBOARD', {type: 'DISMISS'});
+    hideKeyboard: function() {
+      return invoke('KEYBOARD', { type: 'DISMISS' });
     },
-    getCurrentPosition: function (text) {
-      return getAsyncData('GEOLOCATION', {type: 'CURRENT_POSITION', text: text});
+    getCurrentPosition: function(text) {
+      return getAsyncData('GEOLOCATION', {
+        type: 'CURRENT_POSITION',
+        text: text
+      });
     },
-    watchPosition: function (text) {
-      return invoke('GEOLOCATION', {type: 'WATCH_POSITION', text: text});
+    watchPosition: function(text) {
+      return invoke('GEOLOCATION', { type: 'WATCH_POSITION', text: text });
     },
-    clearWatch: function (text) {
-      return invoke('GEOLOCATION', {type: 'CLEAR_WATCH', text: text});
+    clearWatch: function(text) {
+      return invoke('GEOLOCATION', { type: 'CLEAR_WATCH', text: text });
     },
-    stopObserving: function (text) {
-      return invoke('GEOLOCATION', {type: 'STOP_OBSERVING', text: text});
+    stopObserving: function(text) {
+      return invoke('GEOLOCATION', { type: 'STOP_OBSERVING', text: text });
     },
-    copy: function (text) {
-      return invoke('CLIPBOARD', {type: 'COPY', text: text});
+    copy: function(text) {
+      return invoke('CLIPBOARD', { type: 'COPY', text: text });
     },
-    paste: function () {
-      return invoke('CLIPBOARD', {type: 'PASTE'});
+    paste: function() {
+      return invoke('CLIPBOARD', { type: 'PASTE' });
     },
-    open: function (text) {
+    open: function(text) {
       return invoke('OPEN_LINK', text);
     },
-    getOrientation: function () {
-      return getAsyncData('ORIENTATION', {type: 'ORIENTATION'});
+    getOrientation: function() {
+      return getAsyncData('ORIENTATION', { type: 'ORIENTATION' });
     },
-    lockToPortrait: function () {
-      return invoke('ORIENTATION', {type: 'LOCK_PORTRAIT'});
+    lockToPortrait: function() {
+      return invoke('ORIENTATION', { type: 'LOCK_PORTRAIT' });
     },
-    lockToLandscape: function () {
-      return invoke('ORIENTATION', {type: 'LOCK_LANDSCAPE'});
+    lockToLandscape: function() {
+      return invoke('ORIENTATION', { type: 'LOCK_LANDSCAPE' });
     },
-    getConnectionInfo: function () {
-      return getAsyncData('NET_INFO', {type: 'CONNECTION_INFO'});
+    getConnectionInfo: function() {
+      return getAsyncData('NET_INFO', { type: 'CONNECTION_INFO' });
     },
-    addNetInfoEventListener: function () {
-      return invoke('NET_INFO', {type: 'ADD_LISTENER'});
+    addNetInfoEventListener: function() {
+      return invoke('NET_INFO', { type: 'ADD_LISTENER' });
     },
-    removeNetInfoEventListener: function () {
-      return invoke('NET_INFO', {type: 'REMOVE_LISTENER'});
+    removeNetInfoEventListener: function() {
+      return invoke('NET_INFO', { type: 'REMOVE_LISTENER' });
     },
-    hideStatusBar: function () {
-      return invoke('STATUS_BAR', {type: 'HIDE'});
+    hideStatusBar: function() {
+      return invoke('STATUS_BAR', { type: 'HIDE' });
     },
-    showStatusBar: function () {
-      return invoke('STATUS_BAR', {type: 'SHOW'});
+    showStatusBar: function() {
+      return invoke('STATUS_BAR', { type: 'SHOW' });
     },
-    addStateListener: function () {
-      return invoke('STATE', {type: 'ADD_LISTENER'});
+    addStateListener: function() {
+      return invoke('STATE', { type: 'ADD_LISTENER' });
     },
-    removeStateListener: function () {
-      return invoke('STATE', {type: 'REMOVE_LISTENER'});
+    removeStateListener: function() {
+      return invoke('STATE', { type: 'REMOVE_LISTENER' });
     },
-    vibrationVibrate: function (duration) {
-      return invoke('VIBRATION', {type: 'VIBRATE', duration: duration});
+    vibrationVibrate: function(duration) {
+      return invoke('VIBRATION', { type: 'VIBRATE', duration: duration });
     },
-    vibrationCancel: function () {
-      return invoke('VIBRATION', {type: 'CANCEL'});
+    vibrationCancel: function() {
+      return invoke('VIBRATION', { type: 'CANCEL' });
     },
-    setBrightnessLevel: function () {
-      return invoke('BRIGHTNESS', {type: 'SET'});
+    setBrightnessLevel: function() {
+      return invoke('BRIGHTNESS', { type: 'SET' });
     },
-    getBrightnessLevel: function () {
-      return getAsyncData('BRIGHTNESS', {type: 'GET'});
+    getBrightnessLevel: function() {
+      return getAsyncData('BRIGHTNESS', { type: 'GET' });
     },
-    getSystemBrightnessLevel: function () {
-      return getAsyncData('BRIGHTNESS', {type: 'GET_SYSTEM'});
+    getSystemBrightnessLevel: function() {
+      return getAsyncData('BRIGHTNESS', { type: 'GET_SYSTEM' });
+    },
+    console: {
+      // assert(test?: boolean, message?: string, ...optionalParams: any[]): void;
+      assert: function(test, message, ...optionalParams) {
+        return invoke('CONSOLE', {
+          type: 'assert',
+          test,
+          message,
+          optionalParams
+        });
+      },
+      // clear(): void;
+      clear: function() {
+        return invoke('CONSOLE', {
+          type: 'clear'
+        });
+      },
+      // count(countTitle?: string): void;
+      count: function(countTitle) {
+        return invoke('CONSOLE', {
+          type: 'count',
+          countTitle
+        });
+      },
+      // debug(message?: any, ...optionalParams: any[]): void;
+      debug: function(message, ...optionalParams) {
+        return invoke('CONSOLE', {
+          type: 'debug',
+          message,
+          optionalParams
+        });
+      },
+      // dir(value?: any, ...optionalParams: any[]): void;
+      dir: function(value, ...optionalParams) {
+        return invoke('CONSOLE', {
+          type: 'dir',
+          value,
+          optionalParams
+        });
+      },
+      // dirxml(value: any): void;
+      dirxml: function(value) {
+        return invoke('CONSOLE', {
+          type: 'dirxml',
+          value
+        });
+      },
+      // error(message?: any, ...optionalParams: any[]): void;
+      error: function(message, ...optionalParams) {
+        return invoke('CONSOLE', {
+          type: 'error',
+          message,
+          optionalParams
+        });
+      },
+      // exception(message?: string, ...optionalParams: any[]): void;
+      exception: function(message, ...optionalParams) {
+        return invoke('CONSOLE', {
+          type: 'exception',
+          message,
+          optionalParams
+        });
+      },
+      // group(groupTitle?: string, ...optionalParams: any[]): void;
+      group: function(groupTitle, ...optionalParams) {
+        return invoke('CONSOLE', {
+          type: 'group',
+          groupTitle,
+          optionalParams
+        });
+      },
+      // groupCollapsed(groupTitle?: string, ...optionalParams: any[]): void;
+      groupCollapsed: function(groupTitle, ...optionalParams) {
+        return invoke('CONSOLE', {
+          type: 'groupCollapsed',
+          groupTitle,
+          optionalParams
+        });
+      },
+      // groupEnd(): void;
+      groupEnd: function() {
+        return invoke('CONSOLE', {
+          type: 'groupEnd'
+        });
+      },
+      // info(message?: any, ...optionalParams: any[]): void;
+      info: function(message, ...optionalParams) {
+        return invoke('CONSOLE', {
+          type: 'info',
+          message,
+          optionalParams
+        });
+      },
+      // log(message?: any, ...optionalParams: any[]): void;
+      log: function(message, ...optionalParams) {
+        return invoke('CONSOLE', {
+          type: 'log',
+          message,
+          optionalParams
+        });
+      },
+      // msIsIndependentlyComposed(element: Element): boolean;
+      msIsIndependentlyComposed: function(element) {
+        return invoke('CONSOLE', {
+          type: 'msIsIndependentlyComposed',
+          element
+        });
+      },
+      // profile(reportName?: string): void;
+      profile: function(reportName) {
+        return invoke('CONSOLE', {
+          type: 'profile',
+          reportName
+        });
+      },
+      // profileEnd(): void;
+      profileEnd: function() {
+        return invoke('CONSOLE', {
+          type: 'profileEnd'
+        });
+      },
+      // select(element: Element): void;
+      select: function(element) {
+        return invoke('CONSOLE', {
+          type: 'select',
+          element
+        });
+      },
+      // table(...data: any[]): void;
+      table: function(data) {
+        return invoke('CONSOLE', {
+          type: 'table',
+          data
+        });
+      },
+      // time(timerName?: string): void;
+      time: function(timerName) {
+        return invoke('CONSOLE', {
+          type: 'time',
+          timerName
+        });
+      },
+      // timeEnd(timerName?: string): void;
+      timeEnd: function(timerName) {
+        return invoke('CONSOLE', {
+          type: 'timeEnd',
+          timerName
+        });
+      },
+      // trace(message?: any, ...optionalParams: any[]): void;
+      trace: function(message, optionalParams) {
+        return invoke('CONSOLE', {
+          type: 'trace',
+          message,
+          optionalParams
+        });
+      },
+      // warn(message?: any, ...optionalParams: any[]): void;
+      warn: function(message, optionalParams) {
+        return invoke('CONSOLE', {
+          type: 'warn',
+          message,
+          optionalParams
+        });
+      }
     }
   };
 
@@ -177,16 +353,16 @@
       Object.defineProperty(window, 'postMessage', {
         configurable: true,
         enumerable: true,
-        get: function () {
+        get: function() {
           return currentPostMessageFn;
         },
-        set: function (fn) {
+        set: function(fn) {
           currentPostMessageFn = fn;
           isReactNativePostMessageReady = true;
           setTimeout(sendQueue, 0);
         }
       });
-      window.postMessage.toString = function () {
+      window.postMessage.toString = function() {
         return String(originalPostMessage);
       };
     }
@@ -199,8 +375,8 @@
   if (!!window.isPhone) {
     awaitPostMessage(); // Call this only once in your Web Code.
   }
-  if (typeof define === "function" && define.amd) {
-    define("DoreClient", [], function () {
+  if (typeof define === 'function' && define.amd) {
+    define('DoreClient', [], function() {
       return DoreClient;
     });
   }
@@ -208,5 +384,9 @@
   if (typeof noGlobal === strundefined) {
     window.DoreClient = DoreClient;
   }
+
+  // TODO: fix console issue
+  window.console = DoreClient.console
+
   return DoreClient;
-}));
+});
--- examples/webview/www/js/controllers.js
+++ examples/webview/www/js/controllers.js
@@ -1,176 +1,175 @@
-angular.module('starter.controllers', [])
-  .controller('DashCtrl', function ($scope, $ionicPlatform) {
+angular
+  .module('starter.controllers', [])
+  .controller('DashCtrl', function($scope, $ionicPlatform) {
     $scope.version = null;
     $scope.text = 'Text Copy';
     $scope.copyText = '';
-
     DoreClient.addStateListener();
-    $ionicPlatform.on('STATE', function (event) {
+    $ionicPlatform.on('STATE', function(event) {
       $scope.state = event.detail.data;
       $scope.$apply();
     });
-    $ionicPlatform.on('STATE', function (event) {
+    $ionicPlatform.on('STATE', function(event) {
       $scope.state = event.detail.data;
       $scope.$apply();
     });
 
-    DoreClient.getAppVersion().then(function (data) {
+    DoreClient.getAppVersion().then(function(data) {
       $scope.version = data.version;
       $scope.$apply();
     });
-    $scope.showToast = function () {
-      DoreClient.showToast('this is a toast')
+    $scope.showToast = function() {
+      DoreClient.showToast('this is a toast');
     };
-    $scope.showToastCenter = function () {
-      DoreClient.showToast('this is a toast', 'long', 'center')
+    $scope.showToastCenter = function() {
+      DoreClient.showToast('this is a toast', 'long', 'center');
     };
-
-    $scope.open = function () {
-      DoreClient.open('https://www.phodal.com/')
+    $scope.open = function() {
+      DoreClient.open('https://www.phodal.com/');
     };
-
-    $scope.copy = function () {
+    $scope.copy = function() {
       DoreClient.copy($scope.text);
     };
-    $scope.paste = function () {
-      $ionicPlatform.on('PASTE', function (event) {
+    $scope.paste = function() {
+      $ionicPlatform.on('PASTE', function(event) {
         $scope.copyText = event.detail.data;
         $scope.$apply();
       });
       DoreClient.paste();
     };
 
-    $scope.getBadge = function () {
-      DoreClient.getBadge()
+    $scope.getBadge = function() {
+      DoreClient.getBadge();
     };
-    $scope.setBadge = function () {
-      DoreClient.setBadge(19)
+    $scope.setBadge = function() {
+      DoreClient.setBadge(19);
     };
-    $scope.addBadge = function () {
-      DoreClient.addBadge(1)
+    $scope.addBadge = function() {
+      DoreClient.addBadge(1);
     };
-    $scope.minusBadge = function () {
-      DoreClient.minusBadge(1)
+    $scope.minusBadge = function() {
+      DoreClient.minusBadge(1);
     };
-    $scope.showDatePicker = function () {
+    $scope.showDatePicker = function() {
       var options = {
         date: '2017-10-22 12:12:12',
         maxDate: '2022-10-22 12:12:12'
       };
-      DoreClient.showDatePicker(options).then(function (data) {
+      DoreClient.showDatePicker(options).then(function(data) {
         $scope.date = data.date;
         $scope.$apply();
-      })
+      });
     };
 
-    $scope.getCurrentPosition = function () {
-      DoreClient.getCurrentPosition().then(function (data) {
+    $scope.getCurrentPosition = function() {
+      DoreClient.getCurrentPosition().then(function(data) {
         $scope.location = data;
         $scope.$apply();
-      })
+      });
     };
-    $scope.watchPosition = function () {
-      $ionicPlatform.on('WATCH_POSITION', function (event) {
+    $scope.watchPosition = function() {
+      $ionicPlatform.on('WATCH_POSITION', function(event) {
         $scope.wPosition = event.detail.data;
         $scope.$apply();
       });
-      DoreClient.watchPosition()
+      DoreClient.watchPosition();
     };
-    $scope.clearWatch = function () {
-      DoreClient.clearWatch()
+    $scope.clearWatch = function() {
+      DoreClient.clearWatch();
     };
-    $scope.stopObserving = function () {
-      DoreClient.stopObserving()
+    $scope.stopObserving = function() {
+      DoreClient.stopObserving();
     };
-    $scope.getOrientation = function () {
-      DoreClient.getOrientation().then(function (data) {
+    $scope.getOrientation = function() {
+      DoreClient.getOrientation().then(function(data) {
         $scope.orientation = data;
         $scope.$apply();
-      })
+      });
     };
-    $scope.lockToLandscape = function () {
+    $scope.lockToLandscape = function() {
       DoreClient.lockToLandscape();
     };
-    $scope.lockToPortrait = function () {
+    $scope.lockToPortrait = function() {
       DoreClient.lockToPortrait();
     };
-    $scope.getConnectionInfo = function () {
-      DoreClient.getConnectionInfo().then(function (data) {
+    $scope.getConnectionInfo = function() {
+      DoreClient.getConnectionInfo().then(function(data) {
         $scope.connectionInfo = data;
         $scope.$apply();
-      })
+      });
     };
-    $scope.addEventListener = function () {
-      $ionicPlatform.on('CONNECTION_CHANGE', function (event) {
+    $scope.addEventListener = function() {
+      $ionicPlatform.on('CONNECTION_CHANGE', function(event) {
         $scope.wConnectionInfo = event.detail.data;
         $scope.$apply();
       });
       DoreClient.addNetInfoEventListener();
     };
-    $scope.removeEventListener = function () {
+    $scope.removeEventListener = function() {
       DoreClient.removeNetInfoEventListener();
     };
-    $scope.hideStatusBar = function () {
+    $scope.hideStatusBar = function() {
       DoreClient.hideStatusBar();
     };
-    $scope.showStatusBar = function () {
+    $scope.showStatusBar = function() {
       DoreClient.showStatusBar();
     };
-    $scope.vibrationVibrate = function () {
+    $scope.vibrationVibrate = function() {
       DoreClient.vibrationVibrate([1000, 2000, 3000]);
     };
-    $scope.vibrationCancel = function () {
+    $scope.vibrationCancel = function() {
       DoreClient.vibrationCancel();
     };
-    $scope.getBrightness = function () {
+    $scope.getBrightness = function() {
       DoreClient.getBrightness();
     };
-    $scope.setBrightness = function () {
+    $scope.setBrightness = function() {
       DoreClient.setBrightness(0.2);
     };
+    $scope.console = console //  can use inline console function after register
   })
 
-  .controller('DeviceCtrl', function ($scope) {
-    $scope.getAppVersion = function () {
-      DoreClient.getAppVersion().then(function (data) {
+  .controller('DeviceCtrl', function($scope) {
+    $scope.getAppVersion = function() {
+      DoreClient.getAppVersion().then(function(data) {
         $scope.version = data.version;
         $scope.$apply();
       });
     };
-    $scope.getUniqueID = function () {
-      DoreClient.getUniqueID().then(function (data) {
+    $scope.getUniqueID = function() {
+      DoreClient.getUniqueID().then(function(data) {
         $scope.uniqueID = data.uniqueID;
         $scope.$apply();
       });
     };
-    $scope.getBrand = function () {
-      DoreClient.getBrand().then(function (data) {
+    $scope.getBrand = function() {
+      DoreClient.getBrand().then(function(data) {
         $scope.brand = data.brand;
         $scope.$apply();
-      })
+      });
     };
-    $scope.getModel = function () {
-      DoreClient.getModel().then(function (data) {
+    $scope.getModel = function() {
+      DoreClient.getModel().then(function(data) {
         $scope.model = data.model;
         $scope.$apply();
-      })
+      });
     };
-    $scope.getSystemName = function () {
-      DoreClient.getSystemName().then(function (data) {
+    $scope.getSystemName = function() {
+      DoreClient.getSystemName().then(function(data) {
         $scope.systemName = data.systemName;
         $scope.$apply();
-      })
+      });
     };
-    $scope.isEmulator = function () {
-      DoreClient.isEmulator().then(function (data) {
+    $scope.isEmulator = function() {
+      DoreClient.isEmulator().then(function(data) {
         $scope.isEmulator = data;
         $scope.$apply();
-      })
+      });
     };
-    $scope.isTablet = function () {
-      DoreClient.isTablet().then(function (data) {
+    $scope.isTablet = function() {
+      DoreClient.isTablet().then(function(data) {
         $scope.isTablet = data;
         $scope.$apply();
-      })
-    }
+      });
+    };
   });
