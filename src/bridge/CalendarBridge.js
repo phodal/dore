64060a3b4ce4d18e77dede0176641713ed410586
Phodal HUANG <h@phodal.com>
2017-12-26 08:00:33 +0800

    [bridge] add calendar

--- src/bridge/CalendarBridge.js
+++ src/bridge/CalendarBridge.js
@@ -6,7 +6,7 @@
  * @example
  *
  * DoreClient.calendarAuthorizationStatus().then(() -> {})
- * DoreClient.calendarAuthorizeEventStore().then(() -> {})
+ * DoreClient.requestCalendarAuthorize().then(() -> {})
  * DoreClient.findCalendarById.then(() -> {})
  * DoreClient.fetchAllCalendar.then(() -> {})
  * DoreClient.saveCalendar.then(() -> {})
@@ -16,8 +16,80 @@
  */
 
 let CalendarBridge = (payload, webView, Calendar) => {
-
-
+  switch (payload.type) {
+    case 'AUTHORIZATION_STATUS':
+      Calendar.authorizationStatus()
+        .then(status => {
+          webView.postMessage(JSON.stringify(status));
+        })
+        .catch(error => {
+          console.log(error);
+          webView.postMessage(JSON.stringify(error));
+        });
+      break;
+    case 'REQUEST_AUTHORIZE':
+      Calendar.authorizeEventStore()
+        .then(status => {
+          webView.postMessage(JSON.stringify(status));
+        })
+        .catch(error => {
+          console.log(error);
+          webView.postMessage(JSON.stringify(error));
+        });
+      break;
+    case 'FIND_CALENDAR_BY_ID':
+      Calendar.findCalendarById(payload.id)
+        .then(status => {
+          webView.postMessage(JSON.stringify(status));
+        })
+        .catch(error => {
+          console.log(error);
+          webView.postMessage(JSON.stringify(error));
+        });
+      break;
+    case 'FETCH_ALL_CALENDAR':
+      Calendar.fetchAllEvents(payload.startDate, payload.endDate, payload.calendars)
+        .then(status => {
+          webView.postMessage(JSON.stringify(status));
+        })
+        .catch(error => {
+          console.log(error);
+          webView.postMessage(JSON.stringify(error));
+        });
+      break;
+    case 'SAVE_EVENT':
+      Calendar.saveEvent(payload.title, payload.settings)
+        .then(status => {
+          webView.postMessage(JSON.stringify(status));
+        })
+        .catch(error => {
+          console.log(error);
+          webView.postMessage(JSON.stringify(error));
+        });
+      break;
+    case 'REMOVE_EVENT_BY_ID':
+      Calendar.removeEvent(payload.id)
+        .then(status => {
+          webView.postMessage(JSON.stringify(status));
+        })
+        .catch(error => {
+          console.log(error);
+          webView.postMessage(JSON.stringify(error));
+        });
+      break;
+    case 'FIND_CALENDARS':
+      Calendar.findCalendars()
+        .then(status => {
+          webView.postMessage(JSON.stringify(status));
+        })
+        .catch(error => {
+          console.log(error);
+          webView.postMessage(JSON.stringify(error));
+        });
+      break;
+    default:
+      console.log("Error, cannot find type:" + payload.type)
+  }
 };
 
 export default CalendarBridge
